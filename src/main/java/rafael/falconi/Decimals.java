2a19b1c1b193a52d4d239af86e0bfdbd212f83ab
Rafael Falconí <rfalconi@unibe.edu.ec>
2020-10-14 13:30:05 -0500

    add transformDecilmalsToUnitrs function and test

--- src/main/java/rafael/falconi/Decimals.java
+++ src/main/java/rafael/falconi/Decimals.java
@@ -2,4 +2,31 @@ package rafael.falconi;
 
 public class Decimals {
 
+    private double decimal, hundredth;
+
+    public Decimals(double decimal, double hundredth) {
+        this.decimal = decimal;
+        this.hundredth = hundredth;
+    }
+
+    public double getDecimal() {
+        return decimal;
+    }
+
+    public void setDecimal(double decimal) {
+        this.decimal = decimal;
+    }
+
+    public double getHundredth() {
+        return hundredth;
+    }
+
+    public void setHundredth(double hundredth) {
+        this.hundredth = hundredth;
+    }
+
+    public double transformDecilmalsToUnitrs() {
+        return 100*(this.decimal + this.hundredth);
+    }
+
 }
--- src/test/java/rafael/falconi/DecimalsTest.java
+++ src/test/java/rafael/falconi/DecimalsTest.java
@@ -1,7 +1,16 @@
 package rafael.falconi;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DecimalsTest {
 
+    private Decimals decimals;
+
+    @Test
+    public void transformDecilmalsToUnitrsTest(){
+        this.decimals= new Decimals(0.1,0.05);
+        assertEquals(15, this.decimals.transformDecilmalsToUnitrs(),0.01);
+    }
 }
